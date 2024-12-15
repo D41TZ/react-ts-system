739ad07eabb7fc5b5e6a83460841e349c46590d0
Lucas Carnevalli Cunha <lucascarnevallicunha.lcc@gmail.com>
2024-12-15 00:02:57 -0300

    Longe de estar concluído. AddUser não funciona

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,24 @@
-console.log('teste');
+import { max_index, usuarios } from '../database/usuarios.json';
+
+type tUsuario = {
+    cod_usuario : number,
+    senha : string,
+    nome : string,
+};
+
+const addUser = ({ cod_usuario, nome, senha } : tUsuario) : void => {
+    if(usuarios.find((usuario : tUsuario) => usuario.cod_usuario === cod_usuario)){
+        console.log('Código de usuário repetido!')
+    }
+    else {
+        try {
+            usuarios.push({ cod_usuario, nome, senha })             // Ta dando pau pq tem o import normal só importa uma cópia imutável. Precisa usat fs e path. Ver chatGPT
+            console.log('Inserção realizada!')
+        }
+        catch {
+            console.log('Algum erro inesperado ocorreu!')
+        }
+    }
+};
+
+addUser({ cod_usuario: 1, nome: 'Lucas Carnevalli Cunha', senha: 'masterkey' })
